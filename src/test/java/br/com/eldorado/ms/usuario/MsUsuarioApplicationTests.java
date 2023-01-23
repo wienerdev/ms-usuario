af2d28349e65be3a67d729983273ee7c34fbdda7
wienerdev <matheuswiener9@gmail.com>
2023-01-23 20:11:27 -0300

    first commit

---
+++ src/test/java/br/com/eldorado/ms/usuario/MsUsuarioApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.eldorado.ms.usuario;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MsUsuarioApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
